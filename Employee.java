037c89c394b7922302dffe0cfdcc4e10212a48c3
jazmina47 <jazminm0477@gmail.com>
2016-09-28 13:23:22 -0500

    Update employee.java
    Added setters and getters

--- Employee.java
+++ Employee.java
@@ -6,14 +6,15 @@ public class Employee {
 	private String lastName;
 	private String title;
 	private String specialty;
-	private String salary;
+	private double salary;
 	
 	//default constructor 
 	public Employee() {	
+		
 	}
 
 	//constructor with arguments 
-	public Employee(String firstName, String lastName, String title, String specialty, String salary) {
+	public Employee(String firstName, String lastName, String title, String specialty, double salary) {
 		
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -22,5 +23,46 @@ public class Employee {
 		this.salary = salary;
 	}
 
+	//setters and getters
+	public String getFirstName() {
+		return firstName;
+	}
 
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
 }
+

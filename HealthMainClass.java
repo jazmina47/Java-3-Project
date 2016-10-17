d9420becd133c25e409d7f9a80852232b7ab047e
cglass01 <cglass078@yahoo.com>
2016-10-17 11:41:38 -0500

    Update HealthMainClass.java
    No longer the starter window.

--- HealthMainClass.java
+++ HealthMainClass.java
@@ -8,36 +8,33 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class HealthMainClass extends Application{
+public class HealthMainClass {
 	
-	AnchorPane anch;
+	static AnchorPane anch;
+	static Stage primaryStage;
 	
-	public void start(Stage primaryStage) {
+	public static void startPatient() {
 		
-		
+		HealthController control = new HealthController();
 		FXMLLoader loads2 = new FXMLLoader();
 		loads2.setLocation(HealthMainClass.class.getResource("HealthCare.fxml"));
 		try{
 			anch = (AnchorPane) loads2.load();
+		
 		}
 		catch(IOException ex1){
 			
 			ex1.printStackTrace();
 			
 		}
+		primaryStage = new Stage();
 		primaryStage.setTitle("HealthCare Clinic Demo");
-		
 		Scene scene = new Scene(anch);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
 	}
 	
-	
-	
 
-	public static void main(String[] args) {
-		launch(args);
-	}
 
 }

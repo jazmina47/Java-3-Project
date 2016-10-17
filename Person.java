f106ed23a647fb9d888186b0254c8ac608f61a30
cglass01 <cglass078@yahoo.com>
2016-10-17 15:36:44 -0500

    Update Person.java
    updated with more fields.

--- Person.java
+++ Person.java
@@ -1,42 +1,132 @@
-package health_care_final_project;
+package healthcareLook;
 
 public class Person {
 	
-	//data fields 
-	private String firstName;
-	private String lastName;
-
-	//construct a default Person object
-	public Person() {
-	}
-
-	//construct a Person object with the specified firstName and lastName
-	public Person(String firstName, String lastName) {
+	String fName;
+	String lName;
+	String ssn;
+	String address;
+	String city;
+	String zipCode;
+	String county;
+	String phone;
+	String dateOfBirth;
+	String gender;
+	String id;
+	
+	public Person(){
 		
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
-
-	//setters and getters
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
 	}
 	
-	/*@Override
-	public String toString(){
-		return "First Name: " + getFirstName() + "," + " Last Name: " + getLastName();
-	}*/
+
+	public Person(String fName, String lName, String ssn, String address, String city, String zipCode, String county,
+			String phone, String dateOfBirth, String gender, String id) {
+		super();
+		this.fName = fName;
+		this.lName = lName;
+		this.ssn = ssn;
+		this.address = address;
+		this.city = city;
+		this.zipCode = zipCode;
+		this.county = county;
+		this.phone = phone;
+		this.dateOfBirth = dateOfBirth;
+		this.gender = gender;
+		this.id = id;
+	}
+
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getCounty() {
+		return county;
+	}
+
+	public void setCounty(String county) {
+		this.county = county;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
 	
 }

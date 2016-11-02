3a6c1575ca78cd566dba31b5ea4aa8b55cf360eb
jazmina47 <jazminm0477@gmail.com>
2016-11-02 18:00:48 -0500

    updated sql file
    changed ID to varchar. Varchar() means its a string that can be equal to or less than the number of characters given. Char() is a string as well, it just has to be exactly the number of characters it is set to. But there are both string datatypes.

--- healthclinic_db.sql
+++ healthclinic_db.sql
@@ -8,16 +8,16 @@
 DROP TABLE IF EXISTS PATIENT;
 
 CREATE TABLE PATIENT (
-	PATIENT_ID int not null AUTO_INCREMENT,
+	PATIENT_ID varchar(5),
 	P_PASSWORD varchar(20) not null,
 	F_NAME varchar(25) not null,
 	L_NAME varchar(25) not null,
-	SSN	char(9) not null,
+	SSN char(9) not null,
 	ADDRESS varchar(30),
 	ADDR_CITY varchar(20),
 	ADDR_ZIP varchar(10),
 	ADDR_COUNTY varchar(20),
-	PHONE_NUM char(10),
+	PHONE_NUM varchar(10),
 	DOB varchar(20),
 	GENDER varchar(20),
 	IMMUN_STATUS varchar(15),
@@ -34,7 +34,7 @@ CREATE TABLE PATIENT (
 
 INSERT INTO PATIENT (PATIENT_ID, P_PASSWORD, F_NAME, L_NAME, SSN, ADDRESS, ADDR_CITY, ADDR_ZIP, ADDR_COUNTY, PHONE_NUM, DOB, GENDER, IMMUN_STATUS, EMRG_CONTACT, EMRG_C_REL, EMGR_PHONE,
 INSURANCE, APPT_DATE, COMPLAINT)
-VALUES (DEFAULT , 'mypsd1234', 'John','Doe','400103400', '123 Elm Street', 'Aurora', '60503', 'Kane', '6302341030', '01-Jan-1990', 'MALE', 'Up to Date','Mary Jane','SPOUSE', '6301020980', 'Medicaid #102023', '10-Apr-2016', 'Abdomal pain');
+VALUES ('00001', 'mypsd1234', 'John','Doe','400103400', '123 Elm Street', 'Aurora', '60503', 'Kane', '6302341030', '01-Jan-1990', 'MALE', 'Up to Date','Mary Jane','SPOUSE', '6301020980', 'Medicaid #102023', '10-Apr-2016', 'Abdomal pain');
 
 
 -- ------------------------------------------------------------------------------------------------------------
@@ -95,7 +95,7 @@ VALUES (DEFAULT, John, Doe, '123 Elm Street', 'Aurora', '60503', 'Kane', '630234
 DROP TABLE IF EXISTS EMPLOYEE;
 
 CREATE TABLE EMPLOYEE (
-	EMP_ID int  not null,
+	EMP_ID varchar(5),
 	PASSWORD varchar(25) not null,
 	FNAME varchar(25) not null,
 	LNAME varchar(25) not null,
@@ -110,5 +110,4 @@ CREATE TABLE EMPLOYEE (
 );
 
 INSERT INTO EMPLOYEE (EMP_ID, PASSWORD, FNAME, LNAME, SSN, DOB, GENDER, TITLE, SPECIALTY, SALARY) 
-VALUES (10001, 'emp01', 'David', 'Cook', '345985100', '04-Mar-1980', 'Male', 'Secretary', 'Front-desk', '$24,000');
-
+VALUES ('10001', 'emp01', 'David', 'Cook', '345985100', '04-Mar-1980', 'Male', 'Secretary', 'Front-desk', '$24,000');
